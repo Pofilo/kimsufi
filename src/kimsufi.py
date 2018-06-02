a1fda524fb401cf0ea2b7b597c4ff3530b6f95b4
pofilo <git@pofilo.fr>
2018-06-02 21:58:00 +0200

    add logger

--- src/kimsufi.py
+++ src/kimsufi.py
@@ -1,11 +1,24 @@
-#!/usr/bin/env python
-# encoding: UTF-8
+'''
+kimsufi.py: kimsufi's main file.
+
+kimsufi: Sends an alert when your kimsufi is available.
+Copyright (C) 2016-2018 pofilo <git@pofilo.fr>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
 
 import json
 import time
 import http1
 import requests
 
+from logger import log, ERROR, WARN, INFO, DEBUG
 
 API_URL = "https://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2"
 REFERENCE = '160sk2'
@@ -13,7 +26,7 @@ ZONES = set(["gra", "rbx", "sbg", "par"])
 
 
 def main():
-    print "Calling kimsufi API..."
+    log(INFO, 'Calling kimsufi API...')
     try:
         response = http1.get(API_URL)
         if response.status == 200:
@@ -21,12 +34,12 @@ def main():
             for item in struct['answer']['availability']:
                 zones = [z['zone'] for z in item['zones'] if z['availability'] not in ('unavailable', 'unknown')]
                 if set(zones).intersection(ZONES) and item['reference'] == REFERENCE:
-                    print "Found available server, sending sms..."
+                    log(INFO, 'Found available server, sending sms...')
                     r = requests.get("PUT YOUR FREE URL HERE")
         else:
-            print "Error calling API: %s %s" % (response.status, response.message)
+            log(ERROR, 'Calling API: "{}" "{}"'.format(response.status, response.message))
     except Exception as e:
-        print "Error calling API: %s" % str(e)
+        log(ERROR, 'Calling API: {}'.format(str(e)))
 
 if __name__ == '__main__':
     main()
---
+++ src/logger.py
@@ -0,0 +1,26 @@
+'''
+logger.py: implementation of a simple logger.
+
+kimsufi: Sends an alert when your kimsufi is available.
+Copyright (C) 2018 pofilo <git@pofilo.fr>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+
+from datetime import datetime
+
+# LEVELS
+ERROR = 'ERROR'
+WARN = 'WARN'
+INFO = 'INFO'
+DEBUG = 'DEBUG'
+
+# Right now, logs are just 'python prints', we should maybe consider implementing a 'real logger'
+def log(level, message):
+	print '{} ({}): {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), level, message)
