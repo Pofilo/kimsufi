286862e85e937684dbc00e239c1a0656d9679692
pofilo <git@pofilo.fr>
2018-06-17 11:46:39 +0200

    Add while loop and signal interruption

--- src/kimsufi.py
+++ src/kimsufi.py
@@ -18,12 +18,23 @@ import time
 import http1
 import argparse
 import telegram
+import signal
 
 import utils
 from logger import log, ERROR, WARN, INFO, DEBUG
 
+running = True
+
+def signalHandler(signal, frame):
+	global running
+	running = False
+	log(DEBUG, 'Ending signal handled, ending the script...')
+
 def main():
 	log(INFO, '--------------------')
+
+	signal.signal(signal.SIGINT, signalHandler)
+	signal.signal(signal.SIGTERM, signalHandler)
 
 	# Parse arguments
 	parser = argparse.ArgumentParser()
@@ -34,38 +45,45 @@ def main():
 	config, configPath = utils.openAndLoadConfig(args)
 	apiUrl = config.get(utils.sectionDefaultName, utils.apiUrlName)
 	idServer = config.get(utils.sectionDefaultName, utils.idServerName)
+	pollingInterval = config.get(utils.sectionDefaultName, utils.pollingIntervalName)
 	zonesDesired = set()
 	for zone in set(config.items(utils.sectionZonesName)):
 		zonesDesired.add(zone[1])
 
 	log(INFO, 'Calling kimsufi API on "{}"'.format(apiUrl))
-	try:
-		response = http1.get(apiUrl)
-		if response.status == 200:
-			struct = json.loads(response.body)
-			for item in struct['answer']['availability']:
-				zones = [z['zone'] for z in item['zones'] if z['availability'] not in ('unavailable', 'unknown')]
-				if set(zones).intersection(zonesDesired) and item['reference'] == idServer:
-					log(INFO, 'Found available server, sending notifications...')
-					if utils.isConfigSection(config, utils.sectionHTTPRequestName):
-						log(DEBUG, 'Sending HTTP request')
-						request = config.get(utils.sectionHTTPRequestName, utils.HTTPRequest)
-						notifResponse = http1.get(request)
-						if notifResponse.status is not 200:
-							log(ERROR, 'Error calling HTTP request: "{}"'.format(request))
-					if utils.isConfigSection(config, utils.sectionEmailName):
-						log(WARN, 'Email is not implemented yet')
-						# TODO
-					if utils.isConfigSection(config, utils.sectionTelegramName):
-						log(DEBUG, 'Sending Telegram message')
-						token = config.get(utils.sectionTelegramName, utils.telegramTokenName)
-						chatID = config.get(utils.sectionTelegramName, utils.telegramChatIDName)
-						bot = telegram.Bot(token)
-						bot.sendMessage(chatID, 'Hurry up, your kimsufi server is available!!')
-		else:
-			log(ERROR, 'Calling API: "{}" "{}"'.format(response.status, response.message))
-	except Exception as e:
-		log(ERROR, 'Calling API: {}'.format(str(e)))
+	while running:
+		try:
+			response = http1.get(apiUrl)
+			if response.status == 200:
+				struct = json.loads(response.body)
+				for item in struct['answer']['availability']:
+					zones = [z['zone'] for z in item['zones'] if z['availability'] not in ('unavailable', 'unknown')]
+					if set(zones).intersection(zonesDesired) and item['reference'] == idServer:
+						log(INFO, 'Found available server, sending notifications...')
+						if utils.isConfigSection(config, utils.sectionHTTPRequestName):
+							log(DEBUG, 'Sending HTTP request')
+							request = config.get(utils.sectionHTTPRequestName, utils.HTTPRequest)
+							notifResponse = http1.get(request)
+							if notifResponse.status is not 200:
+								log(ERROR, 'Error calling HTTP request: "{}"'.format(request))
+						if utils.isConfigSection(config, utils.sectionEmailName):
+							log(WARN, 'Email is not implemented yet')
+							# TODO
+						if utils.isConfigSection(config, utils.sectionTelegramName):
+							log(DEBUG, 'Sending Telegram message')
+							token = config.get(utils.sectionTelegramName, utils.telegramTokenName)
+							chatID = config.get(utils.sectionTelegramName, utils.telegramChatIDName)
+							bot = telegram.Bot(token)
+							bot.sendMessage(chatID, 'Hurry up, your kimsufi server is available!!')
+			else:
+				log(ERROR, 'Calling API: "{}" "{}"'.format(response.status, response.message))
+					# If signal occurs during process, there is no need to sleep
+			if running:
+				time.sleep(float(pollingInterval))
+		except Exception as e:
+			log(ERROR, 'Calling API: {}'.format(str(e)))
+	
+	log(INFO, 'kimsufi script ended.')
 
 if __name__ == '__main__':
 	main()
